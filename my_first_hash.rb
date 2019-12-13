0aeab7157848164344d4f7a58845d75ca484a179
Tammy Lam <lamtammy1515@gmail.com>
2019-12-13 19:03:33 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,8 +8,8 @@ def my_hash
 def shipping_manifest(array)
  
  shipping_manifest = {
- shipping_manifest["whale bone corsets"]=5
-  shipping_manifest["oil paintings"]=3
+ "whale bone corsets"]=5
+ "oil paintings"]=3
   shipping_manifest["porcelain vases"]=2
  }
 
