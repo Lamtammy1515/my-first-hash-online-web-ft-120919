2d5cdf768697dbfc5729090978824bb9b1c41ccb
Tammy Lam <lamtammy1515@gmail.com>
2019-12-13 19:09:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,9 +9,9 @@ def shipping_manifest(array)
  
  shipping_manifest = {}
 
-  shipping_manifest["whale bone corsets"]=5
-  shipping_manifest["oil paintings"]=3
-  shipping_manifest["porcelain vases"]=2
+  shipping_manifest["whale bone corsets"]
+  shipping_manifest["oil paintings"]
+  shipping_manifest["porcelain vases"]
 
 
   # set a variable called `the_manifest`, equal to a hash
