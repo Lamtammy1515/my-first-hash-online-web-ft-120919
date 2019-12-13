f55142e2ca2a4a27715930fa3f8b6ee6927f8b5c
Tammy Lam <lamtammy1515@gmail.com>
2019-12-13 19:39:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,10 +8,7 @@ def my_hash
  end
 
 
- "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
- 
+ def shipping_manifest
 
   the_manifest = {}
   shipping_manifest["whale bone corsets"]=5
