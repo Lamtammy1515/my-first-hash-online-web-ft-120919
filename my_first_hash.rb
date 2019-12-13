c34a637f595757a779b722e21235fe36436b403c
Tammy Lam <lamtammy1515@gmail.com>
2019-12-13 18:52:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,9 +18,12 @@ def shipping_manifest(array)
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
+end 
 
-return shipping_manifest.to_a
+return shipping_manifest.keys
+return shipping_manifest.values
 end
+
 
 
 def retrieval
